3b5678840688900fede3c31d11b65f0cb54e22ff
Fernando Aguilar <ferrivas127@gmail.com>
2021-01-26 18:27:51 -0600

    Create a login route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+// import dotenv from 'dotenv';
 import Users from '../models/users';
 
 const router = express.Router();
@@ -25,6 +27,39 @@ router.post('/signup', async (req, res) => {
     // Create new user
     const userDB = await Users.create(body);
     return res.json({ status: 'OK', userDB });
+  } catch (error) {
+    return res.json({
+      status: 'Bad',
+      message: 'Bad request',
+      error,
+    });
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  const { body } = req;
+
+  try {
+    const userDB = await Users.findOne({ email: body.email });
+
+    if (!userDB) {
+      return res.json({ status: 'OK', message: 'User or Password incorrect' });
+    }
+
+    if (!bcrypt.compareSync(body.password, userDB.password)) {
+      return res.json({ status: 'OK', message: 'User or Password incorrect' });
+    }
+
+    // Generate token
+    const token = jwt.sign({
+      data: userDB,
+    }, 'pipperperry', { expiresIn: '24h' });
+
+    return res.json({
+      userDB,
+      token,
+    });
   } catch (error) {
     return res.json({
       status: 'Bad',
