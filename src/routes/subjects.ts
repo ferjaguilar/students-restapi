1bc96f152c4770f014833d86856e0724fb166e86
Fernando Aguilar <ferrivas127@gmail.com>
2021-01-30 11:43:48 -0600

    Create a edit subject route

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import _ from 'underscore';
 import Subjects from '../models/subjects';
 
 const router = express.Router();
@@ -38,6 +39,31 @@ router.get('/get-subject/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const subjectDB = await Subjects.find({ _id: id });
+    return res.json({ status: 'OK', subjectDB });
+  } catch (error) {
+    return res.json({
+      status: 'Bad',
+      message: 'Bad request',
+      error,
+    });
+  }
+});
+
+// Edit subject
+router.put('/edit-subject/:id', async (req, res) => {
+  const { id } = req.params;
+  const body = _.pick(req.body, ['code', 'subject', 'teachers', 'schedule', 'students']);
+  try {
+    // Comparation
+    const findSubject = await Subjects.findOne({ _id: id });
+
+    // Validation
+    if (!findSubject) {
+      return res.json({ status: 'OK', message: 'Subject do not found' });
+    }
+
+    // Edit Subject
+    const subjectDB = await Subjects.findByIdAndUpdate({ _id: id }, body, { new: true });
     return res.json({ status: 'OK', subjectDB });
   } catch (error) {
     return res.json({
