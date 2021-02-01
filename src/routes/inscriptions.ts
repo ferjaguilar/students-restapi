2be4cae37a4a686c5ca7bb40027bb7aa85c17451
Fernando Aguilar <ferrivas127@gmail.com>
2021-02-01 10:13:05 -0600

    Create a get all inscription route

--- src/routes/inscriptions.ts
+++ src/routes/inscriptions.ts
@@ -19,4 +19,20 @@ router.post('/add-inscriptions', async (req, res) => {
   }
 });
 
+// Get all inscriptions
+router.get('/get-inscriptions', async (req, res) => {
+  try {
+    const inscriptionsDB = await Inscriptions.find({ status: true })
+      .populate('student', 'code name lastName')
+      .populate('subjects', 'code subject schedule');
+    return res.json({ status: 'OK', inscriptionsDB });
+  } catch (error) {
+    return res.json({
+      status: 'Bad',
+      message: 'Bad request',
+      error,
+    });
+  }
+});
+
 module.exports = router;
